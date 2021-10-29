94e7fb495dd460b68c2e521b47147c927ad04266
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-10-29 14:32:29 -0700

    API successfully sends calls, has PUT and DELETE routes, contains SeedData. Took some debugging, errors with localhost delayed project

---
+++ Animal/Migrations/20211029213143_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Male", "Ruffus", "Dog" },
+                    { 2, 1, "Male", "Fuzz Aldrin", "Cat" },
+                    { 3, 2, "Female", "Pawpstar", "Dog" },
+                    { 4, 2, "Female", "DangerNoodle", "Ferret" },
+                    { 5, 10, "Male", "Kats Meow", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
--- Animal/Models/AnimalShelterContext.cs
+++ Animal/Models/AnimalShelterContext.cs
@@ -10,5 +10,16 @@ namespace AnimalShelter.Models
         }
 
         public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        builder.Entity<Animal>().HasData(
+        new Animal { AnimalId = 1, Name = "Ruffus", Species = "Dog", Age = 7, Gender = "Male" },
+        new Animal { AnimalId = 2, Name = "Fuzz Aldrin", Species = "Cat", Age = 1, Gender = "Male" },
+        new Animal { AnimalId = 3, Name = "Pawpstar", Species = "Dog", Age = 2, Gender = "Female" },
+        new Animal { AnimalId = 4, Name = "DangerNoodle", Species = "Ferret", Age = 2, Gender = "Female" },
+        new Animal { AnimalId = 5, Name = "Kats Meow", Species = "Cat", Age = 10, Gender = "Male" }
+            );
+        }
     }
 }

3be9d3bb3d78ad216b9c33a23c423922b7d69224
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-10-29 14:55:36 -0700

    contain swagger functionality

--- Animal/Startup.cs
+++ Animal/Startup.cs
@@ -23,10 +23,13 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {c.RoutePrefix = "swagger"; c.SwaggerEndpoint("/swagger/v1/swagger.json", "test");} );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,7 +40,7 @@ namespace AnimalShelter
             app.UseRouting();
 
             app.UseAuthorization();
-
+    
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
